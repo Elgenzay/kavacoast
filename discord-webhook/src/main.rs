4e19bd0bf8c46924edaae2eae78f5324428e6b81
Jason <jasonkrabs1@gmail.com>
2022-11-24 21:28:32 -0500

    Create bot, webhook, & api files

--- discord-webhook/src/main.rs
+++ discord-webhook/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-	println!("Hello, world!");
+use std::collections::HashMap;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+	let mut map = HashMap::new();
+	map.insert("username", "KavaBot");
+	map.insert("content", "test");
+
+	dotenv::dotenv().ok();
+	let webhook_url =
+		std::env::var("WEBHOOK_URL").expect("Missing environment variable: WEBHOOK_URL");
+	let icon_url = std::env::var("ICON_URL").expect("Missing environment variable: ICON_URL");
+	map.insert("avatar_url", &icon_url[..]);
+
+	let client = reqwest::blocking::Client::new();
+	let resp = client.post(webhook_url).json(&map).send();
+	println!("{:#?}", resp);
+	Ok(())
 }
