27428ce4b2a77ea9b067496073ac7db675594741
Jason <jason@elg.gg>
2022-11-25 08:02:46 -0500

    Add static directory functionality

--- rest-api/src/main.rs
+++ rest-api/src/main.rs
@@ -1,12 +1,26 @@
-#[macro_use]
-extern crate rocket;
+use rocket::fs::{relative, FileServer};
 
-#[get("/")]
+mod manual {
+	use rocket::fs::NamedFile;
+	use std::path::{Path, PathBuf};
+
+	#[rocket::get("/<path..>")]
+	pub async fn second(path: PathBuf) -> Option<NamedFile> {
+		let mut path = Path::new(super::relative!("static")).join(path);
+		if path.is_dir() {
+			path.push("index.html");
+		}
+		NamedFile::open(path).await.ok()
+	}
+}
+#[rocket::get("/api")]
 fn index() -> &'static str {
-	"test"
+	"!test"
 }
 
-#[launch]
+#[rocket::launch]
 fn rocket() -> _ {
-	rocket::build().mount("/", routes![index])
+	rocket::build()
+		.mount("/", rocket::routes![manual::second])
+		.mount("/", rocket::routes![index])
 }
