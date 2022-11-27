4b44e044bac4985c84270c29fa9d89e3a80c5870
Jason <jason@elg.gg>
2022-11-26 20:47:14 -0500

    simplify/trim the rest api file

--- rest-api/src/main.rs
+++ rest-api/src/main.rs
@@ -1,26 +1,24 @@
-use rocket::fs::{relative, FileServer};
+use rocket::fs::relative;
+use rocket::fs::NamedFile;
+use std::path::{Path, PathBuf};
 
-mod manual {
-	use rocket::fs::NamedFile;
-	use std::path::{Path, PathBuf};
-
-	#[rocket::get("/<path..>")]
-	pub async fn second(path: PathBuf) -> Option<NamedFile> {
-		let mut path = Path::new(super::relative!("static")).join(path);
-		if path.is_dir() {
-			path.push("index.html");
-		}
-		NamedFile::open(path).await.ok()
+#[rocket::get("/<path..>")]
+pub async fn static_pages(path: PathBuf) -> Option<NamedFile> {
+	let mut path = Path::new(relative!("static")).join(path);
+	if path.is_dir() {
+		path.push("index.html");
 	}
+	NamedFile::open(path).await.ok()
 }
+
 #[rocket::get("/api")]
-fn index() -> &'static str {
+fn api() -> &'static str {
 	"!test"
 }
 
 #[rocket::launch]
 fn rocket() -> _ {
 	rocket::build()
-		.mount("/", rocket::routes![manual::second])
-		.mount("/", rocket::routes![index])
+		.mount("/", rocket::routes![static_pages])
+		.mount("/", rocket::routes![api])
 }
