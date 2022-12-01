5f3cac087fbd4d2e4944798df7d4bbc5f4f177ef
Jason <jason@elg.gg>
2022-11-30 22:26:18 -0500

    Move urls/ips to env vars/gitignore

--- https-redirect/src/main.rs
+++ https-redirect/src/main.rs
@@ -3,12 +3,15 @@ use std::path::PathBuf;
 
 #[rocket::get("/<path..>")]
 pub async fn redirect(path: PathBuf) -> Redirect {
-	let mut new_uri = String::from("https://kava.elg.gg/");
+	let mut new_uri = String::from("https://");
+	let domain = std::env::var("DOMAIN").expect("Missing environment variable: DOMAIN");
+	new_uri.push_str(&domain);
 	new_uri.push_str(&path.into_os_string().into_string().unwrap());
 	Redirect::to(new_uri)
 }
 
 #[rocket::launch]
 fn rocket() -> _ {
+	dotenvy::dotenv().ok();
 	rocket::build().mount("/", rocket::routes![redirect])
 }
